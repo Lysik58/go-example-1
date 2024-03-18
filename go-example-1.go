a2548ad5f94c498ce0aa7490d67fdd5c949a93c4
Denis Lysenkov <msprodenis@mail.ru>
2024-03-19 02:30:34 +0300

    First commit second commit

--- go-example-1.go
+++ go-example-1.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	defer fmt.Println("End of the program")
+	var result int
+	fmt.Println("Start of the program")
+	//var counter = makeCounter(0)
+
+	for i := 0; i < 10; i++ {
+		result = counter
+		if result > 5 {
+			fmt.Println("Break")
+			break
+		}
+		fmt.Println(result)
+	}
 }
